722ba4eb0291ba1579581a99b4ca7983f65423e3
Lovelynchristian <78356777+Lovelynchristian@users.noreply.github.com>
2021-05-18 10:59:20 +0100

    added a newline

---
+++ day_4.py
@@ -0,0 +1,17 @@
+#Project on Rock Paper Scissors 
+import random 
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for paper or 2 for Scissors."))
+
+computer_choice = random.randint(0, 2)
+print(f"Computer chose {computer_choice}")
+
+if user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice > user_choice:
+    print("You lose")
+elif computer_choice == user_choice:
+    print("It's a draw")
+else:
+    print("You typed an invalid number, you lose!")
+    
